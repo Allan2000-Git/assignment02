dd63e618107e59ba539405fe593ee52473e195b5
Allan Joston Fernandes <allanfernandes459@gmail.com>
2023-11-25 22:13:49 +0530

    first commit

---
+++ src/containers/home/InterviewSettingsForm.tsx
@@ -0,0 +1,108 @@
+import { Button, Flex, Box } from "@chakra-ui/react";
+import React, { useEffect } from "react";
+import { useFormik } from "formik";
+
+import FormSelect from "../../components/formComponents/FormSelect";
+import { IInterViewSettings } from "../../interface/forms";
+import {
+  interviewDurationOptions,
+  interviewLanguageOptions,
+  interviewModeOptions,
+} from "./constants";
+import { useData } from "./DataProvider";
+
+const InterviewDetailsForm: React.FC = ({setCurrentTab}:any) => {
+  const { setState, state }: any = useData();
+
+  const changeState = () =>{
+    setState((prevState: any) => ({
+      ...prevState,
+      interviewSettings: values,
+    }));
+  }
+
+  const {
+    errors,
+    touched,
+    handleSubmit,
+    values,
+    setFieldTouched,
+    setFieldValue,
+  } = useFormik<IInterViewSettings>({
+    initialValues: {
+      interviewMode: "",
+      interviewDuration: "",
+      interviewLanguage: "",
+    },
+    onSubmit: (values) => {
+      changeState()
+      alert("Form successfully submitted");
+    },
+  });
+
+  useEffect(()=>{
+    changeState()
+  },[values, setState])
+
+  useEffect(() => {
+    // Pre-fill form fields with stored values
+    setFieldValue("interviewMode", state.interviewSettings?.interviewMode);
+    setFieldValue("interviewDuration", state.interviewSettings?.interviewDuration);
+    setFieldValue("interviewLanguage", state.interviewSettings?.interviewLanguage);
+
+    // Mark the fields as touched to trigger validation messages if needed
+    setFieldTouched("interviewMode", true);
+    setFieldTouched("interviewDuration", true);
+    setFieldTouched("interviewLanguage", true);
+  }, [state.interviewSettings, setFieldValue, setFieldTouched]);
+
+  return (
+    <Box width="100%" as="form" onSubmit={handleSubmit as any}>
+      <Box width="100%">
+        <FormSelect
+          label="Interview Mode"
+          placeholder="Select interview mode"
+          name="interviewMode"
+          options={interviewModeOptions}
+          onChange={setFieldValue}
+          onBlur={setFieldTouched}
+          value={values?.interviewMode}
+          error={errors?.interviewMode}
+          touched={touched?.interviewMode}
+        />
+        <FormSelect
+          label="Interview Duration"
+          placeholder="Select interview duration"
+          name="interviewDuration"
+          options={interviewDurationOptions}
+          onChange={setFieldValue}
+          onBlur={setFieldTouched}
+          value={values?.interviewDuration}
+          error={errors?.interviewDuration}
+          touched={touched?.interviewDuration}
+        />
+        <FormSelect
+          label="Job Location"
+          name="interviewLanguage"
+          placeholder="Select interview language"
+          options={interviewLanguageOptions}
+          onChange={setFieldValue}
+          onBlur={setFieldTouched}
+          error={errors.interviewLanguage}
+          touched={touched.interviewLanguage}
+          value={values.interviewLanguage}
+        />
+        <Flex w="100%" justify="flex-end" mt="4rem" gap="20px">
+          <Button onClick={() => setCurrentTab((prev:any) => prev-1)} colorScheme="gray" type="button">
+            Previous
+          </Button>
+          <Button colorScheme="red" type="submit">
+            Submit
+          </Button>
+        </Flex>
+      </Box>
+    </Box>
+  );
+};
+
+export default InterviewDetailsForm;
---
+++ src/containers/home/JobDetailsForm.tsx
@@ -0,0 +1,103 @@
+import { Button, Flex, Box } from "@chakra-ui/react";
+import React, { useEffect } from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
+import FormInput from "../../components/formComponents/FormInput";
+import { IJobDetails } from "../../interface/forms";
+import { useData } from "./DataProvider";
+
+const JobDetailsForm: React.FC = ({setCurrentTab}: any) => {
+
+  const { setState, state }: any = useData();
+
+  const changeState = () =>{
+    setState((prevState: any) => ({
+      ...prevState,
+      jobDetails: values,
+    }));
+  }
+
+  const { handleChange, errors, touched, handleBlur, handleSubmit, values, setFieldValue, setFieldTouched } =
+    useFormik<IJobDetails>({
+      initialValues: {
+        jobTitle: "",
+        jobDetails: "",
+        jobLocation: "",
+      },
+      validationSchema: Yup.object().shape({
+        jobTitle: Yup.string().required("Job Title is required"),
+        jobDetails: Yup.string().required("Job Details is required"),
+        jobLocation: Yup.string().required("Job Location is required"),
+        jobPosition: Yup.string().required("Job position is required"),
+      }),
+      onSubmit: (values) => {
+        changeState()
+        setCurrentTab(2);
+      },
+    });
+
+    useEffect(() => {
+      changeState()
+    }, [values, setState]);
+
+    useEffect(() => {
+      // Pre-fill form fields with stored values
+      setFieldValue("jobTitle", state.jobDetails?.jobTitle);
+      setFieldValue("jobDetails", state.jobDetails?.jobDetails);
+      setFieldValue("jobLocation", state.jobDetails?.jobLocation);
+  
+      // Mark the fields as touched to trigger validation messages if needed
+      setFieldTouched("jobTitle", true);
+      setFieldTouched("jobDetails", true);
+      setFieldTouched("jobLocation", true);
+    }, [state.jobDetails, setFieldValue, setFieldTouched]);
+
+  return (
+
+    <Box width="100%" as="form" onSubmit={handleSubmit as any}>
+      <Box width="100%">
+        <FormInput
+          label="Job Title"
+          placeholder="Enter job title"
+          name="jobTitle"
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={values?.jobTitle}
+          error={errors?.jobTitle}
+          touched={touched?.jobTitle}
+        />
+        <FormInput
+          label="Job Details"
+          placeholder="Enter job details"
+          name="jobDetails"
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={values?.jobDetails}
+          error={errors?.jobDetails}
+          touched={touched?.jobDetails}
+        />
+        <FormInput
+          label="Job Location"
+          name="jobLocation"
+          placeholder="Enter job location"
+          onChange={handleChange}
+          onBlur={handleBlur}
+          error={errors.jobLocation}
+          touched={touched.jobLocation}
+          value={values.jobLocation}
+        />
+        <Flex w="100%" justify="flex-end" mt="4rem" gap="20px">
+          <Button onClick={() => setCurrentTab((prev:any) => prev-1)} colorScheme="gray" type="button">
+            Previous
+          </Button>
+          <Button onClick={() => setCurrentTab((prev:any)=>{prev+1})} colorScheme="red" type="submit">
+            Next
+          </Button>
+        </Flex>
+      </Box>
+    </Box>
+  );
+};
+
+export default JobDetailsForm;
